1d0b3807d016076a5fcfada4a3212d8f97e8e073
berkeleycole <alyssamhope@gmail.com>
2019-06-15 14:21:11 -0700

    adds aylien api

--- src/public/index.js
+++ src/public/index.js
@@ -2,9 +2,9 @@ function handleSubmit(event) {
     event.preventDefault()
 
     console.log("clicked!")
-    fetch('http://localhost:3000/test')
+    fetch('http://localhost:3000/api')
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results').innerHTML = res.remaining
     })
 }
--- src/server.js
+++ src/server.js
@@ -3,11 +3,20 @@ const express = require('express')
 const app = express()
 app.use(express.static('src/public'));
 
+var aylien = require("aylien_textapi");
+
 var json = {
     'title': 'test json response',
     'message': 'this is a message',
     'time': 'now'
 }
+
+// set aylien API credentias
+var textapi = new aylien({
+  application_id: "652cef67",
+  application_key: "b1d451f9bd9418ebd2f9bd892057ee0a"
+});
+
 
 app.get('/', function (req, res) {
     res.sendFile('/views/index.html', { root: __dirname });
@@ -15,6 +24,13 @@ app.get('/', function (req, res) {
 
 app.get('/test', function (req, res) {
     res.send(json);
+})
+
+app.get('/api', function(req, res) {
+    textapi.sentiment({'text': 'John is a very good football player'}, function(err, result, remaining) {
+        console.log(remaining)
+        res.send(remaining)
+    });
 })
 
 // designates what port the app will listen to for incoming requests
